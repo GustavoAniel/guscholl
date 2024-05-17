083dfb5cfae781c2fff24c786efb7235181e4af4
GustavoAniel <aniel.amtimil@gmail.com>
2024-05-16 22:22:45 -0300

    Cria layout e adiciona lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { Atom, Pen } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900">
-      <p>Teste</p>
+    <main className="flex min-h-screen flex-col items-center p-24 bg-slate-900">
+      <h1 className="font-bold text-white text-2xl">Home</h1>
+      <div className="flex flex-1 flex-col items-center justify-center w-full gap-4">
+        <Link className="text-white flex justify-center gap-2 font-bold bg-emerald-500 w-72 text-center py-8 rounded-xl" href="/login/aluno">
+          Login Aluno
+          <Pen />
+        </Link>
+        <Link className="text-white flex justify-center gap-2 font-bold bg-yellow-500 w-72 text-center py-8 rounded-xl" href="/login/professor">
+          Login Professor
+          <Atom />
+        </Link>
+      </div>
     </main>
   );
 }
