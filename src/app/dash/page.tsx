ee5196c79687acb7d5fc1a9fdd7b388e9465bb2c
GustavoAniel <aniel.amtimil@gmail.com>
2024-05-16 21:10:59 -0300

    Bump

---
+++ src/app/dash/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+export default function Login(){
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900">
+      <h1 className="text-white font-bold text-2xl">DASHBOARD</h1>
+    </main>
+  )
+}
