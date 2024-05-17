554d0444e0179f00fed65783c7f7c3f8c3a3886d
GustavoAniel <aniel.amtimil@gmail.com>
2024-05-17 18:00:29 -0300

    Altera layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-slate-900">
-      <h1 className="font-bold text-white text-2xl">Home</h1>
       <div className="flex flex-1 flex-col items-center justify-center w-full gap-4">
-        <Link className="text-white flex justify-center gap-2 font-bold bg-emerald-500 w-72 text-center py-8 rounded-xl" href="/login/aluno">
-          Login Aluno
-          <Pen />
-        </Link>
-        <Link className="text-white flex justify-center gap-2 font-bold bg-yellow-500 w-72 text-center py-8 rounded-xl" href="/login/professor">
-          Login Professor
-          <Atom />
+        <Link className="text-white flex justify-center gap-2 font-bold bg-emerald-500 w-72 text-center py-8 rounded-xl" href="/login">
+          Login
         </Link>
       </div>
     </main>
