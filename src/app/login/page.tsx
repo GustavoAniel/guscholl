f23dc509ab098f583c6d5b975ba424badaf64e60
GustavoAniel <aniel.amtimil@gmail.com>
2024-06-04 22:42:12 -0300

    Adiciona handle submit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -13,8 +14,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import Router from "next/router"
-import { CircleUserRound, User } from "lucide-react"
+import { CircleUserRound, LoaderCircle } from "lucide-react"
 
 const formSchema = z.object({
   cpf: z.string().length(11, {
@@ -26,6 +26,7 @@ const formSchema = z.object({
 })
 
 export default function Login() {
+  const [onLogin, setOnLogin] = useState(false)
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -36,49 +37,66 @@ export default function Login() {
 
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-  
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setOnLogin(true)
+    if(values){
+      await fetch("/api/get-login", {
+      }).then((response) => {
+        response.json().then((data) => {
+          console.log(data)
+        })
+      })
+    }
+    setOnLogin(false)
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-900">
-      <CircleUserRound size={80} className="text-emerald-500 mb-4"/>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex flex-col">
-          <FormField
-            control={form.control}
-            name="cpf"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel className="text-white">CPF</FormLabel>
+      {onLogin ?
+        <div className="flex flex-col items-center text-white gap-2">
+          <h1>Carregando</h1>
+          <LoaderCircle className="animate-spin" size={30}/>
+        </div>
+      :
+        <div className="flex flex-col items-center">
+        <CircleUserRound size={80} className="text-emerald-500 mb-4"/>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex flex-col">
+            <FormField
+              control={form.control}
+              name="cpf"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-white">CPF</FormLabel>
+                  <FormControl className="outline-none pl-2">
+                    <input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="senha"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                <FormLabel className="text-white">Senha</FormLabel>
                 <FormControl className="outline-none pl-2">
-                  <input {...field} />
+                  <input type="password" {...field} />
                 </FormControl>
+                
                 <FormMessage />
               </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="senha"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-              <FormLabel className="text-white">Senha</FormLabel>
-              <FormControl className="outline-none pl-2">
-                <input type="password" {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-            )}
-          >
+              )}
+            >
 
-          </FormField>
-          <Button type="submit" className="bg-emerald-500 hover:bg-emerald-800">Entrar</Button>
-        </form>
-      </Form>
+            </FormField>
+            <Button type="submit" className="bg-emerald-500 hover:bg-emerald-800">Entrar</Button>
+          </form>
+        </Form>
+        </div>
+      
+      }
     </main>
   )
 }
